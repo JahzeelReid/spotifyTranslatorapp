f56c1709564ffa3adfe7ae7a1161f89ce3e97765
JahzeelReid <102478869+JahzeelReid@users.noreply.github.com>
2024-11-06 18:00:09 -0500

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -3,8 +3,7 @@ from spotipy.oauth2 import SpotifyOAuth
 
 # Set up Spotify client credentials
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id='0bab9090c39a47159f1a7fe883297d35',
-    client_secret='17fbf11ae69e408baeecf64f66d7e996',
+    
     redirect_uri='http://localhost:8888/callback',
     scope="user-read-currently-playing"
 ))
